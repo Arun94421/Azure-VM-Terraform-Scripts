62ca9106cbf11c46c67a139df2543854b6fc96da
Arun <arunadhithya10@gmail.com>
2025-10-30 10:06:44 +0530

    Create Results.tf

---
+++ Terraform Modules/Virtual Network/Results.tf
@@ -0,0 +1,11 @@
+# results.tf
+
+output "vnet_id" {
+  description = "The ID of the created virtual network."
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+  description = "The ID of the created subnet."
+  value       = azurerm_subnet.subnet.id
+}
