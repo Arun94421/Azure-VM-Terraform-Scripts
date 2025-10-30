6f83a9e8155ea1c42a415c9d409d6da868fc44fa
Arun <arunadhithya10@gmail.com>
2025-10-30 09:20:01 +0530

    Update results.tf

--- Terraform Modules/Resource Group/results.tf
+++ Terraform Modules/Resource Group/results.tf
@@ -1,1 +1,20 @@
+# modules/resource_group/results.tf
+
+# Output the name of the created resource group
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+  description = "The name of the Azure Resource Group"
+}
+
+# Output the location of the created resource group
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+  description = "The location where the Azure Resource Group was created"
+}
+
+# Output the ID of the created resource group
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
+  description = "The ID of the Azure Resource Group"
+}
 
